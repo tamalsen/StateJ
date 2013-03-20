8806b03038284d3cda2d2a026f1731f58d3e9c00
tamalsen <tml.cse@gmail.com>
2013-03-20 10:54:15 +0530

    deleted

--- src/com/srts/phaseI/util/StringUtils.java
+++
@@ -1,9 +0,0 @@
-package com.srts.phaseI.util;
-
-public interface StringUtils {
-
-	public static final String VALUE_UNKNOWN="##UNKNOWN##";
-	public static final String NULL="##NULL##";
-	public static final String EXCEPTION = "##EXCEPTION##";
-	
-}
